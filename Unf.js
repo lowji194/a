b3f314410842da0f17bd18eb4f544479812e8279
lowji194 <theloi194@gmail.com>
2021-08-29 08:51:27 +0700

    Create Unf.js

---
+++ Unf.js
@@ -0,0 +1,117 @@
+setTimeout(function() {
+if (Key == 25072021) {
+    var total = prompt("Số bạn bè muốn xóa", "1000");
+	var number = 5; // Thời gian nghỉ
+
+	var speed = getRndInteger(number, number);
+	var time = speed * 1000; // Don't modify
+
+	function getRndInteger(min, max) {
+	    return Math.floor(Math.random() * max) + min;
+	}
+
+	function getRandomInt(max) {
+	    return Math.floor(Math.random() * Math.floor(max));
+	}
+
+	document.body.style.zoom = "50%"
+	setTimeout(function() {
+	    window.scrollTo(0, document.body.scrollHeight);
+	}, time * 2);
+	console.clear();
+	// Don't modify code below
+	    console.log("%c Script Auto Deletes Friends Facebook 2021",'color: #008000');
+	    console.log("%c Copyright belong to Lowji194",'color: #008000');
+	    console.log("%c Contact Support: www.facebook.com/100005942927037",'color: #008000');
+	    console.warn("%c Times Delays: " + number + "-" + (number + number) + " seconds",'color: #ff0000');
+	    console.warn("%c Total Request: " + total,'color: #ff0000');
+	    console.warn("%c The process will start later: " + Math.round((number / 3)) + " seconds",'color: #ff0000');
+	    console.log("--------------------------------");
+	var block = 0;
+	var send = 0;
+
+	var Lowji194 = document.getElementsByClassName("bp9cbjyn ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi n1f8r23x rq0escxv j83agx80");
+	var del = document.getElementsByClassName("oajrlxb2");
+	var check = document.getElementsByClassName('cjfnh4rs l9j0dhe7 du4w35lb j83agx80 cbu4d94t lzcic4wl ni8dbmo4 stjgntxs oqq733wu cwj9ozl2 io0zqebd m5lcvass fbipl8qg nwvqtn77 nwpbqux9 iy3k6uwz e9a99x49 g8p4j16d bv25afu3 gc7gaz0o k4urcfbm');
+	function add(n) {
+	    if (block == 1) {
+	        return document.tile = ('Đã bị chặn tính năng');
+	    }
+	    if (send >= total) {
+	        window.location.href = 'http://facebook.com/profile.php';
+
+	    }
+	        /* if (check[0].getAttribute('aria-label') == 'Giờ bạn chưa dùng được tính năng này') {
+	             ++block
+	         } */
+	    setTimeout(function continuousWhenPageLoad() {
+	        if (50 >= Lowji194.length) {
+	            window.scrollTo(0, document.body.scrollHeight);
+	            console.warn("%c Auto Scroll Enable",'color: #008000',`${Lowji194.length}`);
+	            setTimeout(continuousWhenPageLoad, 3000);
+	            return;
+	        }
+	        if (Lowji194[n].lastChild.lastChild.lastChild === null || Lowji194[n].lastChild.lastChild.lastChild.innerText == 'Thêm bạn bè') {
+	            console.log(`%c Bỏ qua ${label}`,'color: #ff0000');
+	            Lowji194[n].remove();
+	            setTimeout(function() {
+	                add(0);
+	            }, 100);
+	        } else {
+	            rd = getRandomInt(Lowji194.length - 1);
+	            Lowji194[n].lastChild.lastChild.lastChild.lastChild.click(); // click add friends
+
+	            setTimeout(function() {
+	                for (var i = 0; i < del.length; i++) {
+	                    if (del[i].innerText == 'Hủy kết bạn') {
+	                        del[i].click();
+	                    }
+	                };
+	                setTimeout(function() {
+						let dels = check[0].getElementsByClassName('oajrlxb2')
+						if (check[0] !== undefined) label = check[0].firstChild.innerText;
+	                    for (var i = 0; i < dels.length; i++) {
+	                        if (dels[i].getAttribute('aria-label') == 'Xác nhận') {
+	                            dels[i].click();
+	                        }
+	                    };
+	                    setTimeout(function() {
+	                        if (Lowji194[n].lastChild.lastChild.lastChild.innerText == 'Thêm bạn bè') {
+	                            console.log(`%c ${label} - ${++send} request [${speed = getRndInteger(number, number)} giây]`,'color: #008000');
+	                            document.title = (`[${send}] ${label}`);
+	                        }
+	                        setTimeout(function() {
+	                            Lowji194[n].remove();
+	                        }, 1000);
+	                    }, 1000);
+	                }, 1000);
+	            }, 1500);
+
+	            setTimeout(function() {
+	                add(rd);
+	            }, time);
+	        }
+	    }, 0);
+	    l = document.getElementsByClassName("oajrlxb2");
+	    setTimeout(function() {
+	        for (var e = 0; e < l.length; e++) {
+	            if (l[e].getAttribute('aria-label') == 'OK') {
+	                l[e].click();
+	            }
+	        }
+	    }, 5000)
+	}
+	setTimeout(function() {
+	    add(0);
+	}, time / 3);
+} else {
+	alert('Key không hợp lệ 🚫, liên hệ lợi nhé');
+}
+}, 1000);
+var url = 'https://locuid.com/API/Reg';
+var Key;
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+Key = out
+})
