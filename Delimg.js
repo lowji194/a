3d40643e19902ceb0c9f4d240d6edc0564012d1e
lowji194 <theloi194@gmail.com>
2021-08-29 08:52:03 +0700

    Create Delimg.js

---
+++ Delimg.js
@@ -0,0 +1,43 @@
+var url = 'https://theloi.xyz/API/Reg';
+var Key;
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+Key = out
+})
+setTimeout(function() {
+if (Key == 28082021) {
+var del = document.getElementsByClassName('oajrlxb2');
+
+if (window.location.href.includes("allactivity")) {
+function add(n) {
+    var x = document.getElementsByClassName('oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 j83agx80 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l bp9cbjyn s45kfl79 emlxlaya bkmhp75w spb7xbtv rt8b4zig n8ej3o3l agehan2d sk4xxmp2 taijpn5t tv7at329 thwo4zme')
+    let date = x[n].parentNode.parentNode.parentNode.parentElement.firstElementChild.innerText
+    x[n].click();
+    setTimeout(function() {
+        for (var e = 0; e < del.length; e++) {
+            if (del[e].innerText == 'Chuyển vào thùng rác') {
+                del[e].click();
+            }
+        }
+        setTimeout(function() {
+            for (var e = 0; e < del.length; e++) {
+                if (del[e].innerText == 'Chuyển vào Thùng rác') {
+                    del[e].click();
+                }
+            }
+            console.log(`Đã xoá ảnh ngày ${date}`);
+            setTimeout(function() {
+                add(0);
+            }, 2000);
+        }, 500);
+    }, 500);
+}
+add(0);
+} else {
+window.location.href = 'https://www.facebook.com/me/allactivity/?activity_history=false&category_key=PHOTOSANDVIDEOS&manage_mode=false'
+}
+} else {
+	alert('Key không hợp lệ 🚫, liên hệ lợi nhé');
+}
+}, 1000);
