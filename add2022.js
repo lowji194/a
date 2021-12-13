e0db9ca65e36b2023d37df6fff8bcc557f2609c7
lowji194 <theloi194@gmail.com>
2021-12-13 12:59:44 +0700

    Update add2022.js

--- add2022.js
+++ add2022.js
@@ -46,7 +46,7 @@
 				<br/>🤝 Đã là bạn bè với <span style='color:green'>${user.name}</span>🙋‍♂️</b>`
 				}
 				else {
-					 if (totalSuccessRequests >= total/4) {
+					 if (totalFailedRequests >= total/4) {
 						document.title = ('[' + totalSuccessRequests + '] Tài khoản Facebook đã bị chặn tính năng') 
 						 break;
 					 } else {
@@ -59,7 +59,7 @@
 				});
 				
 			}
-			if (totalSuccessRequests >= total/4) parentElement.innerHTML = `👌 Hoàn thành kết bạn.
+			if (totalFailedRequests >= total/4) parentElement.innerHTML = `👌 Hoàn thành kết bạn.
 			<br/>Thành công: ${totalSuccessRequests} ✔️ - Lỗi: ${totalFailedRequests} 🚫
 			<br/><span style='color:red'>Đã Bị Chặn Tính Năng</span>`
 			else parentElement.innerHTML = `👌 Hoàn thành kết bạn.
