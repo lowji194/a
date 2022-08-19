eae9c3b004bb189a50fd5e128d10416e2438120d
lowji194 <theloi194@gmail.com>
2022-08-19 10:28:13 +0700

    Update Add1.js

--- Add1.js
+++ Add1.js
@@ -27,7 +27,7 @@
 	if (window.location.href.includes("groups")) {
 	    var Lowji194 = document['getElementsByClassName']('rq0escxv l9j0dhe7 du4w35lb hybvsw6c io0zqebd m5lcvass fbipl8qg nwvqtn77 k4urcfbm ni8dbmo4 stjgntxs sbcfpzgs')[0].getElementsByClassName('ue3kfks5 pw54ja7n uo3d90p7 l82x9zwi a8c37x1j');
 	} else {
-	    var Lowji194 = document['getElementsByClassName']('i85zmo3j h8391g91 m0cukt09 kpwa50dg ta68dy8c o5pw47cy bdao358l alzwoclg mfn553m3 g4qalytl r227ecj6 ez8dtbzv gt60zsk1 hgcwkpcn');
+	    var Lowji194 = document['getElementsByClassName']('i85zmo3j h8391g91 m0cukt09 kpwa50dg ta68dy8c o5pw47cy bdao358l alzwoclg mfn553m3');
 		if (document.querySelectorAll("div[aria-label='Thêm bạn bè']")[0] == undefined == false) document.querySelectorAll("div[aria-label='Thêm bạn bè']")[0].click();
 	}
 
